bec0fc8a2632d31467c9c5faa3cdbf21f59899ff
alibachi15 <alibachipvt@gmail.com>
2026-07-12 21:51:59 +0530

    Initial commit

---
+++ floatdouble.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+int main()
+{
+float f=12.3;
+double d=1234.567;
+
+printf("f=%f\ni",f);
+
+printf("f=%.2f\n",f);
+
+printf("f=%.3f\n",f);
+
+
+printf("d=%lf\n",d);
+
+return 0;
+}
